1ddb46937a7597fd0fa982196df0769ee5928726
kasturada1 <kazuki1.katsurada@gmail.com>
2024-06-26 01:23:28 +0900

    Set up a sleeping barber project

---
+++ sleeping_barber/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+// variables
+var seatingCapacity = 10
+var arrivalRate = 100
+var cutDuration = 1000 * time.Millisecond
+var timeOpen = 10 * time.Second
+
+func main() {
+	// seed our random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// print welcome message
+	color.Yellow("The Sleeping Barber Problem")
+	color.Yellow("---------------------------")
+
+	// create channels if we need any
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
+
+	// create the barbershop
+	shop := BarberShop{
+		ShopCapacity:    seatingCapacity,
+		HairCutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan:     clientChan,
+		BarbersDoneChan: doneChan,
+		Open:            true,
+	}
+
+	color.Green("The shop is open for the day!")
+
+	// add barbers
+
+	// start the barbershop as a goroutine
+
+	// add clients
+
+	// block until the barbershop is closed
+}
