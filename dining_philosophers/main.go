a2f76498f6afe4f160dc8c150c96e1e9f8b0a461
kasturada1 <kazuki1.katsurada@gmail.com>
2024-06-25 17:46:31 +0900

    Add implemention for the dining logic

--- dining_philosophers/main.go
+++ dining_philosophers/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"sync"
 	"time"
 )
 
+// Philosopher is a struct which stores information about a philosopher.
 type Philosopher struct {
 	name      string
 	rightFork int
@@ -27,11 +29,18 @@ var eatTime = 1 * time.Second   // how long it takes to eatTime
 var thinkTime = 3 * time.Second // how long a philosopher thinks
 var sleepTime = 1 * time.Second // how long to wait when printing things out
 
+// *** added this
+var orderMutex sync.Mutex  // a mutex for the slice orderFinished; part of challenge!
+var orderFinished []string // the order in which philosophers finish dining and leave; part of challenge!
+
 func main() {
 	// print out a welcome message
 	fmt.Println("Dining Philosophers Problem")
 	fmt.Println("---------------------------")
 	fmt.Println("The table is empty.")
+
+	// *** added this
+	time.Sleep(sleepTime)
 
 	// start the meal
 	dine()
@@ -39,9 +48,17 @@ func main() {
 	// print out finished message
 	fmt.Println("The table is empty.")
 
+	// *** added this
+	time.Sleep(sleepTime)
+	fmt.Printf("Order finished: %s.\n", strings.Join(orderFinished, ", "))
+
 }
 
 func dine() {
+	eatTime = 0 * time.Second
+	sleepTime = 0 * time.Second
+	thinkTime = 0 * time.Second
+
 	// wg is the WaitGroup that keeps track of how many philosophers are still at the table. When
 	// it reaches zero, everyone is finished eating and has left. We add 5 (the number of philosophers) to this
 	// wait group.
@@ -75,4 +92,55 @@ func dine() {
 // until everyone is seated at the table.
 func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
 	defer wg.Done()
+
+	// seat the philosopher at the table
+	fmt.Printf("%s is seated at the table.\n", philosopher.name)
+
+	// Decrement the seated WaitGroup by one.
+	seated.Done()
+
+	// Wait until everyone is seated.
+	seated.Wait()
+
+	// Have this philosopher eatTime and thinkTime "hunger" times (3).
+	for i := hunger; i > 0; i-- {
+		// Get a lock on the left and right forks. We have to choose the lower numbered fork first in order
+		// to avoid a logical race condition, which is not detected by the -race flag in tests; if we don't do this,
+		// we have the potential for a deadlock, since two philosophers will wait endlessly for the same fork.
+		// Note that the goroutine will block (pause) until it gets a lock on both the right and left forks.
+		if philosopher.leftFork > philosopher.rightFork {
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+		} else {
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+		}
+
+		// By the time we get to this line, the philosopher has a lock (mutex) on both forks.
+		fmt.Printf("\t%s has both forks and is eating.\n", philosopher.name)
+		time.Sleep(eatTime)
+
+		// The philosopher starts to think, but does not drop the forks yet.
+		fmt.Printf("\t%s is thinking.\n", philosopher.name)
+		time.Sleep(thinkTime)
+
+		// Unlock the mutexes for both forks.
+		forks[philosopher.leftFork].Unlock()
+		forks[philosopher.rightFork].Unlock()
+
+		fmt.Printf("\t%s put down the forks.\n", philosopher.name)
+	}
+
+	// The philosopher has finished eating, so print out a message.
+	fmt.Println(philosopher.name, "is satisified.")
+	fmt.Println(philosopher.name, "left the table.")
+
+	// *** added this
+	orderMutex.Lock()
+	orderFinished = append(orderFinished, philosopher.name)
+	orderMutex.Unlock()
 }
